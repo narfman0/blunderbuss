e96f5a9e3fa26cbe1c28543a91d88f824da6650a
Jon Robison <narfman0@blastedstudios.com>
2024-01-14 10:19:46 -0500

    add movement

--- blunderbuss/main.py
+++ blunderbuss/main.py
@@ -4,22 +4,22 @@ import time
 import pygame
 
 from blunderbuss.map import Map
+from blunderbuss.models import Entity
+from blunderbuss.settings import *
 from blunderbuss.util.logging import initialize_logging
 
 LOGGER = logging.getLogger(__name__)
-FRAMERATE = 1/60
-WIDTH = 1024
-HEIGHT = 768
-SURFACE_SCALAR = 3
+
 
 def main():
     pygame.init()
-    pygame.display.set_caption('blunderbuss')
-    screen = pygame.display.set_mode((WIDTH, HEIGHT),0,32)
+    pygame.display.set_caption("blunderbuss")
+    screen = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
     display = pygame.Surface((WIDTH // SURFACE_SCALAR, HEIGHT // SURFACE_SCALAR))
     initialize_logging()
     running = True
     gamemap = Map()
+    player = Entity()
 
     while running:
         for event in pygame.event.get():
@@ -29,8 +29,12 @@ def main():
             if event.key == pygame.K_ESCAPE:
                 running = False
 
-        display.fill((0,0,0))
-        gamemap.draw(display, screen)
+        keys = pygame.key.get_pressed()
+        player.x += (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * SPEED
+        player.y += (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * SPEED
+
+        display.fill((0, 0, 0))
+        gamemap.draw(display, screen, player)
         pygame.display.update()
         time.sleep(FRAMERATE)
 
--- blunderbuss/map.py
+++ blunderbuss/map.py
@@ -2,27 +2,35 @@ import pygame
 
 from pygame.locals import *
 
+from blunderbuss.models import Entity
+from blunderbuss.settings import *
+
+CAMERA_OFFSET_X = (WIDTH // SURFACE_SCALAR) // 2
+CAMERA_OFFSET_Y = (HEIGHT // SURFACE_SCALAR) // 2
 
 class Map:
     def __init__(self):
-        self.grass_img = pygame.image.load('data/images/map/grass.png').convert()
+        self.grass_img = pygame.image.load("data/images/map/grass.png").convert()
         self.grass_img.set_colorkey((0, 0, 0))
-        self.stone_img = pygame.image.load('data/images/map/stone.png').convert()
+        self.stone_img = pygame.image.load("data/images/map/stone.png").convert()
         self.stone_img.set_colorkey((0, 0, 0))
 
-        f = open('data/map.txt')
-        self.map_data = [[int(c) for c in row] for row in f.read().split('\n')]
+        f = open("data/map.txt")
+        self.map_data = [[int(c) for c in row] for row in f.read().split("\n")]
         f.close()
 
-    def draw(self, display: pygame.Surface, screen: pygame.Surface):
-            for y, row in enumerate(self.map_data):
-                for x, tile in enumerate(row):
-                    blit_coords = (150 + x * 10 - y * 10, 100 + x * 5 + y * 5)
-                    blit_image = None
-                    if tile == 1:
-                        blit_image = self.grass_img
-                    elif tile == 2:
-                        blit_image = self.stone_img
-                    if blit_image:
-                        display.blit(blit_image, blit_coords)
-            screen.blit(pygame.transform.scale(display, screen.get_size()), (0, 0))
+    def draw(self, display: pygame.Surface, screen: pygame.Surface, player: Entity):
+        for y, row in enumerate(self.map_data):
+            for x, tile in enumerate(row):
+                blit_coords = (
+                    CAMERA_OFFSET_X + x * 10 - y * 10 - player.x,
+                    CAMERA_OFFSET_Y + x * 5 + y * 5 - player.y,
+                )
+                blit_image = None
+                if tile == 1:
+                    blit_image = self.grass_img
+                elif tile == 2:
+                    blit_image = self.stone_img
+                if blit_image:
+                    display.blit(blit_image, blit_coords)
+        screen.blit(pygame.transform.scale(display, screen.get_size()), (0, 0))
