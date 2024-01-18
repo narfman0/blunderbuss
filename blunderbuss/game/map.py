f689f0e7094fc28e0bbfacd31e9dbbdfb629e923
Jon Robison <narfman0@gmail.com>
2024-01-18 09:41:08 -0500

    clean unused map methods. add startxy for player spawning

--- blunderbuss/game/map.py
+++ blunderbuss/game/map.py
@@ -16,21 +16,7 @@ class Map:
         self.tmxdata = pytmx.load_pygame(f"data/tiled/{area}.tmx")
 
     def get_start_tile(self):
-        return map(int, self.tmxdata.properties.get("StartTile").split(","))
-
-    def get_tile_type(self, tile_x, tile_y, layer):
-        tile_props = self.tmxdata.get_tile_properties(tile_x, tile_y, layer) or {}
-        return tile_props.get("type")
-
-    def get_transition_details(self, tile_x, tile_y):
-        object = self.tmxdata.get_object_by_name(f"transition,{tile_x},{tile_y}")
-        destination_area = object.properties["Destination"]
-        dest_x, dest_y = map(int, object.properties["DestinationXY"].split(","))
-        return TransitionDetails(destination_area, dest_x, dest_y)
-
-    def has_colliders(self, tile_x, tile_y, layer):
-        tile_props = self.tmxdata.get_tile_properties(tile_x, tile_y, layer) or {}
-        return tile_props.get("colliders")
+        return map(int, self.tmxdata.properties.get("StartXY").split(","))
 
     def get_tile_image(self, tile_x, tile_y, layer):
         return self.tmxdata.get_tile_image(tile_x, tile_y, layer)
--- blunderbuss/game/world.py
+++ blunderbuss/game/world.py
@@ -1,15 +1,36 @@
+from pygame.math import Vector2
+
 from blunderbuss.game.map import Map
 from blunderbuss.game.models import Character, Direction
 
 SPEED = 5
-
+TILE_X_SCALAR = 256
+TILE_Y_SCALAR = 128
 
 class World:
     def __init__(self):
         self.map = Map("level1")
-        self.player = Character()
+        tile_x, tile_y = self.map.get_start_tile()
+        x, y = self.tile_coords_to_world_coordinates(tile_x, tile_y)
+        self.player = Character(position=Vector2(x, y))
 
     def move_player(self, dt: float, direction: Direction):
         if direction:
             self.player.direction = direction
             self.player.position += direction.to_vector() * SPEED
+
+    @classmethod
+    def tile_coords_to_world_coordinates(
+        cls, tile_x: int, tile_y: int
+    ):
+        x = (
+            (tile_x * TILE_X_SCALAR) // 2
+            - (tile_y * TILE_X_SCALAR) // 2
+            - TILE_X_SCALAR // 2
+        )
+        y = (
+            (tile_x * TILE_Y_SCALAR) // 2
+            + (tile_y * TILE_Y_SCALAR) // 2
+            - TILE_Y_SCALAR // 2
+        )
+        return (x, y)
