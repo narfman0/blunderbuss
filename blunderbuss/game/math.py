aeb1303215413da6d285f769530d40efc17584eb
Jon Robison <narfman0@blastedstudios.com>
2024-01-19 16:46:33 -0500

    Add math module :Sweat-smile:

---
+++ blunderbuss/game/math.py
@@ -0,0 +1,9 @@
+from pygame.math import Vector2
+
+def cartesian_to_isometric(cartesian: Vector2) -> Vector2:
+    return Vector2(cartesian.x - cartesian.y, (cartesian.x + cartesian.y) // 2)
+
+
+def isometric_to_cartesian(isometric: Vector2) -> Vector2:
+    cartesian_x = (isometric.x + isometric.y * 2) // 2
+    return Vector2(cartesian_x, cartesian_x + isometric.x)
