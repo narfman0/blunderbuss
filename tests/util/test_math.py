d05db3b7343419b878923bc6cec02abce2c25151
Jon Robison <narfman0@gmail.com>
2024-01-24 16:50:20 -0500

    lol, i typod the test condition. this was working.

--- tests/util/test_math.py
+++ tests/util/test_math.py
@@ -6,9 +6,9 @@ class TestMath(unittest.TestCase):
     def test_point_in_polygon_middle(self):
         vertx = [0, 10, 10, 0]
         verty = [0, 0, 10, 10]
-        self.assertEqual(True, math.point_in_polygon(vertx, verty, 5, 5))
+        self.assertTrue(math.point_in_polygon(vertx, verty, 5, 5))
 
     def test_point_in_polygon_outside(self):
         vertx = [0, 10, 10, 0]
         verty = [0, 0, 10, 10]
-        self.assertEqual(True, math.point_in_polygon(vertx, verty, 150, 150))
+        self.assertFalse(math.point_in_polygon(vertx, verty, 150, 150))
