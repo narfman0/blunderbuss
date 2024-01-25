6f676bf2e1257eb5573287e91da95d14b4b12300
Jon Robison <narfman0@blastedstudios.com>
2024-01-25 18:41:33 -0500

    make movement a bit more crisp

--- blunderbuss/game/world.py
+++ blunderbuss/game/world.py
@@ -3,7 +3,10 @@ import pymunk
 from blunderbuss.game.map import Map
 from blunderbuss.game.models import Character, Direction
 
-SPEED = 200
+RUN_FORCE = 25000
+RUNNING_STOP_THRESHOLD = 2
+MAX_VELOCITY = 5
+NPC_MASS = 10
 
 
 class World:
@@ -14,17 +17,22 @@ class World:
         self.player = Character()
         self.player.body.position = (0.5 + tile_x, 0.5 + tile_y)
         self.player.poly = pymunk.Circle(self.player.body, 1)
-        self.player.poly.mass = 10
+        self.player.poly.mass = NPC_MASS
         self.space.add(self.player.body, self.player.poly)
         self.map.add_map_geometry_to_space(self.space)
 
     def move_player(self, dt: float, direction: Direction):
         if direction:
             self.player.direction = direction
-            dpos = direction.to_vector() * SPEED * dt
-            self.player.body.velocity = dpos.x, dpos.y
+            dpos = direction.to_vector() * RUN_FORCE * dt
+            self.player.body.apply_force_at_local_point(force=(dpos.x, dpos.y))
+            if self.player.body.velocity.length > MAX_VELOCITY:
+                self.player.body.velocity = self.player.body.velocity.scale_to_length(MAX_VELOCITY)
         else:
-            self.player.body.velocity = 0, 0
+            if self.player.body.velocity.get_length_sqrd() > RUNNING_STOP_THRESHOLD:
+                self.player.body.velocity = self.player.body.velocity.scale_to_length(0.7*self.player.body.velocity.length)
+            else:
+                self.player.body.velocity = (0,0)
 
     def update(self, dt: float):
         self.space.step(dt)
