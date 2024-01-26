1cd3ba4da4ba768a3d3d6b18f01205b8e15d192d
Jon Robison <narfman0@gmail.com>
2024-01-26 12:59:08 -0500

    add cyberstreets tileset

---
+++ data/tiled/cyberstreets.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="cyberstreets" tilewidth="32" tileheight="32" tilecount="160" columns="16">
+ <image source="cyberstreets/cyberstreets_tiles_export.png" width="512" height="320"/>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="17.25"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16.25" y="18"/>
+  </objectgroup>
+ </tile>
+</tileset>
