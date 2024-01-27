09839205ab1480f6833d3e72b8320044560ad25e
Jon Robison <narfman0@gmail.com>
2024-01-27 15:03:53 -0500

    implement dash

--- blunderbuss/game/models.py
+++ blunderbuss/game/models.py
@@ -5,7 +5,9 @@ from pygame import Vector2
 import pymunk
 
 CHARACTER_MASS = 10
-DASH_DURATION = 2.0
+DASH_COOLDOWN = 0.25
+DASH_DURATION = 0.25
+DASH_SCALAR = 3
 RUN_FORCE = 25000
 RUNNING_STOP_THRESHOLD = 2
 MAX_VELOCITY = 5
@@ -54,6 +56,7 @@ class Character:
     body: pymunk.Body = None
     dashing: bool = False
     dash_time_remaining: float = 0
+    dash_cooldown_remaining: float = 0
 
     def __init__(self, position: tuple[float, float]):
         self.body = pymunk.Body()
@@ -67,13 +70,19 @@ class Character:
             if self.dash_time_remaining <= 0:
                 self.dashing = 0
                 self.dash_time_remaining = 0
+                self.dash_cooldown_remaining = DASH_COOLDOWN
+        elif self.dash_cooldown_remaining > 0:
+            self.dash_cooldown_remaining -= dt
+            if self.dash_cooldown_remaining <= 0:
+                self.dash_cooldown_remaining = 0
         if self.input_direction:
             self.facing_direction = self.input_direction
-            dpos = self.input_direction.to_vector() * RUN_FORCE * dt
+            dash_scalar = DASH_SCALAR if self.dashing else 1.0
+            dpos = self.input_direction.to_vector() * RUN_FORCE * dash_scalar * dt
             self.body.apply_force_at_local_point(force=(dpos.x, dpos.y))
-            if self.body.velocity.length > MAX_VELOCITY:
+            if self.body.velocity.length > MAX_VELOCITY * dash_scalar:
                 self.body.velocity = self.body.velocity.scale_to_length(
-                    MAX_VELOCITY
+                    MAX_VELOCITY * dash_scalar
                 )
         else:
             if self.body.velocity.get_length_sqrd() > RUNNING_STOP_THRESHOLD:
@@ -84,8 +93,9 @@ class Character:
                 self.body.velocity = (0, 0)
 
     def dash(self):
-        self.dashing = True
-        self.dash_time_remaining = DASH_DURATION
+        if not self.dashing and self.dash_cooldown_remaining <= 0:
+            self.dashing = True
+            self.dash_time_remaining = DASH_DURATION
 
     @property
     def position(self):
